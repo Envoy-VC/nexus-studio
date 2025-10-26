d4a4385f27af0a7c455c22efa04b8e55f948768a
Vedant Chainani <vedantchainani1084@gmail.com>
2025-10-26 21:25:44 +0530

    chore: add nav logo

--- apps/web/src/components/navbar.tsx
+++ apps/web/src/components/navbar.tsx
@@ -1,11 +1,15 @@
 "use client";
 
+import { NexusLogo } from "@nexus-studio/ui/icons";
 import { ConnectKitButton } from "connectkit";
 
 export const Navbar = () => {
   return (
     <div className="flex h-[6dvh] flex-row items-center justify-between border-b bg-gray-50 px-4">
-      <div>Nexus Studio</div>
+      <div className="flex flex-row items-center gap-2">
+        <NexusLogo className="size-7" />
+        <div className="font-semibold text-gray-700 text-xl">Nexus Studio</div>
+      </div>
       <ConnectKitButton />
     </div>
   );
